b887bb48e9b28487994c4ec4ab924b99ca49c686
Chris Schepman <cschep@simple.com>
2013-04-28 15:04:19 -0700

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ketten::Application.config.secret_token = 'a3ca816bbc472692fb2b298b81e1b5dd20c4105f8dd6f0fbe8d8ecbd76321d8102e129a17a9edcf91f4a988d74cf174a429b224e80ceb4bf2b535c0498882a1b'
