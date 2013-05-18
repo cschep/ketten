81a6c355d8debe0d9c9630a51f4a294c96565377
Chris Schepman <cschep@simple.com>
2013-05-18 14:05:10 -0700

    remove comments

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,12 +1,3 @@
-# root = "/home/deployer/apps/ketten/current"
-# pid "/home/cschep/code/ruby/ketten/tmp/pids/unicorn.pid"
-# stderr_path "/home/cschep/code/ruby/ketten/unicorn/unicorn.log"
-# stdout_path "/home/cschep/code/ruby/ketten/unicorn/unicorn.log"
-
-# listen "/tmp/unicorn.todo.sock"
-# worker_processes 2
-# timeout 30
-
 root = "/home/deployer/apps/ketten/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
