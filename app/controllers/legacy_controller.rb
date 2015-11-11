dc789fbabc26d39a3cae0fc45750e5c11682bbd9
Chris Schepman <cschep@gmail.com>
2015-11-10 19:00:17 -0600

    legacy controller improvements

--- app/controllers/legacy_controller.rb
+++ app/controllers/legacy_controller.rb
@@ -1,19 +1,4 @@
 class LegacyController < ApplicationController
-
-  def message_or
-    the_ketten = User.where(:ketten => true).first
-    message = the_ketten.live_message
-
-    render :json => { :message => message.content }
-  end
-
-  def message_wa
-    the_ketten = User.where(:ketten => true).first
-    message = the_ketten.live_message
-
-    render :json => { :message => message.content }
-  end
-
   def json
     @songs = searchdb(params[:search], params[:searchby])
     render :json => @songs.to_json(:only => [:artist, :title]), :root => false
@@ -26,11 +11,15 @@ class LegacyController < ApplicationController
 
   def random
     the_ketten = User.where(:ketten => true).first
-    songbook = the_ketten.default_songbook
-    @songs = songbook.songs.all(:order => 'random()', :limit => 20)
-    render :json => @songs.to_json(:only => [:artist, :title]), :root => false
+    if the_ketten && the_ketten.default_songbook
+      songbook = the_ketten.default_songbook
+      @songs = songbook.songs.all(:order => 'random()', :limit => 20)
+      render :json => @songs.to_json(:only => [:artist, :title]), :root => false
 
-    Search.create(:search_term => "random", :search_by => "random", :user_agent => request.user_agent, :num_results => @songs.count, :ip_address => request.ip)
+      Search.create(:search_term => "random", :search_by => "random", :user_agent => request.user_agent, :num_results => @songs.count, :ip_address => request.ip)
+    else
+      render :json => [].to_json(), :root => false
+    end
   end
 
   def stats
@@ -38,19 +27,24 @@ class LegacyController < ApplicationController
   end
 
   def searchdb(search_term, search_by)
-    the_ketten = User.where(:ketten => true).first
-    songbook = the_ketten.default_songbook
+    @songs = []
 
-    puts "searching for #{search_term} by #{search_by} in songbook with id: #{songbook.id} and name: #{songbook.name}"
+    if search_by && search_term
+      the_ketten = User.where(:ketten => true).first
+      if the_ketten && the_ketten.default_songbook
+        songbook = the_ketten.default_songbook
 
-    @songs = songbook.search(search_term, search_by)
+        @songs = songbook.search(search_term, search_by)
 
-    Search.create(:search_term => search_term,
-                  :search_by => search_by,
-                  :user_agent => request.user_agent,
-                  :num_results => @songs.count,
-                  :ip_address => request.ip,
-                  :songbook_id => songbook.id)
+        Search.create(:search_term => search_term,
+                      :search_by => search_by,
+                      :user_agent => request.user_agent,
+                      :num_results => @songs.count,
+                      :ip_address => request.ip,
+                      :songbook_id => songbook.id)
+
+      end
+    end
 
     @songs
   end
