40985ee21beea846ce273062653e8e4c89c85054
Chris Schepman <cschep@gmail.com>
2020-07-17 19:28:37 -0700

    rails 6.0.3.2

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '5.2'
+gem 'rails', '6.0.3.2'
 gem 'bootstrap-sass', '2.3.2'
 
 # Bundle edge Rails instead:
--- db/schema.rb
+++ db/schema.rb
@@ -1,76 +1,74 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110235256) do
+ActiveRecord::Schema.define(version: 2020_07_18_022456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "messages", force: :cascade do |t|
-    t.text     "content"
+  create_table "messages", id: :serial, force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "live"
-    t.integer  "user_id"
+    t.boolean "live"
+    t.integer "user_id"
   end
 
-  create_table "searches", force: :cascade do |t|
-    t.string   "search_term"
-    t.string   "search_by"
-    t.string   "user_agent"
-    t.integer  "num_results"
-    t.string   "ip_address"
+  create_table "searches", id: :serial, force: :cascade do |t|
+    t.string "search_term"
+    t.string "search_by"
+    t.string "user_agent"
+    t.integer "num_results"
+    t.string "ip_address"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "songbook_id"
+    t.integer "songbook_id"
   end
 
-  create_table "songbooks", force: :cascade do |t|
-    t.string   "name"
+  create_table "songbooks", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
-    t.string   "songbook"
-    t.boolean  "default"
+    t.integer "user_id"
+    t.string "songbook"
+    t.boolean "default"
   end
 
-  create_table "songs", force: :cascade do |t|
-    t.string   "artist"
-    t.string   "title"
+  create_table "songs", id: :serial, force: :cascade do |t|
+    t.string "artist"
+    t.string "title"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "songbook_id"
+    t.integer "songbook_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer "sign_in_count", default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.boolean  "ketten"
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "ketten"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
