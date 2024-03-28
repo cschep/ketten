3f795f4df9c518584b7b332a37877d5232ef97a2
Chris Schepman <chris@schepman.org>
2024-03-28 00:38:13 -0400

    cleanup initializers

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,2 @@
-# frozen_string_literal: true
-
 Time::DATE_FORMATS[:month_and_year] = '%B %Y'
 Time::DATE_FORMATS[:pretty] = ->(time) { time.strftime('%a, %b %e at %l:%M') + time.strftime('%p').downcase }
